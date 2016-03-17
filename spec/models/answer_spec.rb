0f9285e42bae9399e658b1bf62e33469b4b32b70
Teddy <teddyhealysmith@gmail.com>
2016-03-17 16:11:00 -0700

    assignment 28 completed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: false) }
+  let(:answer) { Answer.create!(body: 'Comment Body', question: question) }
+
+   describe "attributes" do
+     it "responds to body" do
+       expect(answer).to respond_to(:body)
+     end
+   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: false) }
+
+   describe "attributes" do
+ # #2
+     it "responds to title" do
+       expect(question).to respond_to(:title)
+     end
+ # #3
+     it "responds to body" do
+       expect(question).to respond_to(:body)
+     end
+
+     it "responds to resolved" do
+       expect(question).to respond_to(:resolved)
+
+     end
+   end
 end
