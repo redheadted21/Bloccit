404a7da51cf5686fced0d7c7868d1fb94947d01a
Teddy <teddyhealysmith@gmail.com>
2016-04-26 15:31:18 -0700

    Checkpoint 33 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    
+
     @post = Post.find(params[:id])
 
   end
@@ -36,5 +36,40 @@ class PostsController < ApplicationController
 
 
   def edit
+
+    @post = Post.find(params[:id])
+
   end
+
+  def update
+
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+       flash[:notice] = "Post was updated successfully."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+
+  end
+
+  def destroy
+
+    @post = Post.find(params[:id])
+
+ 
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+
+  end
+
 end
