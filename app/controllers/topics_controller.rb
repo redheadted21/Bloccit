ba8e2d00b634c2a7b4b47bf015e3dfc9a64687bc
Teddy <teddyhealysmith@gmail.com>
2016-05-06 14:36:41 -0700

    marking progress

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,9 +13,31 @@ class TopicsController < ApplicationController
   end
 
   def new
+
+    @topic = Topic.new
+
   end
 
+  def create
+
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+       redirect_to @topic
+     else
+       flash.now[:alert] = "Error creating topic. Please try again."
+       render :new
+     end
+   end
+
   def edit
+
+    @topic = Topic.find(params[:id])
+
   end
 
 
