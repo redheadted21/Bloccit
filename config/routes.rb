c37004f5cadc180beaaef530f92f4b9532ee0a94
Teddy <teddyhealysmith@gmail.com>
2016-05-09 14:10:47 -0700

    marking progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :topics
+  resources :topics do
 
-  resources :posts
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
