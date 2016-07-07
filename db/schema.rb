dc8e368ca14f593c77ac35d22194f100c2ae593a
Xander Miller <xander.miller@gmail.com>
2016-07-07 15:29:24 +0000

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20160518225241) do
 
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -31,6 +39,14 @@ ActiveRecord::Schema.define(version: 20160518225241) do
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
